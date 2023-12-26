ff1aee688a76f29577976404cd0289ef611e4f2c
Joji Augustine <jojimail@gmail.com>
2023-12-26 19:38:07 +0000

    Delete 7 files

--- dist/Root.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Root = void 0;
-const jsx_runtime_1 = require("react/jsx-runtime");
-function Root(props) {
-    return ((0, jsx_runtime_1.jsxs)("html", { children: [(0, jsx_runtime_1.jsx)("head", { children: (0, jsx_runtime_1.jsx)("title", { children: "React server components" }) }), (0, jsx_runtime_1.jsx)("body", { children: (0, jsx_runtime_1.jsx)("div", { id: "root", children: props.children }) })] }));
-}
-exports.Root = Root;
--- dist/client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const client_1 = require("react-dom/client");
-const App_1 = require("./App");
-(0, client_1.hydrateRoot)(document.getElementById("root"), (0, jsx_runtime_1.jsx)(App_1.App, {}));
